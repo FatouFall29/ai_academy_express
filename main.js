faa3ded09bc2d597450f9e9e99b762b12ce68e66
FatouFall29 <fallf5784@gmail.com>
2025-04-17 11:52:22 +0100

    modifications du fichier

--- main.js
+++ main.js
@@ -1,132 +1,10 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const path = require("path");
-// const expressLayouts = require("express-ejs-layouts");
-// const session = require("express-session");
-// // Ajoutez les contrôleurs
-// const subscribersController = require("./controllers/subscribersController");
-// const homeController = require("./controllers/homeController");
-// const errorController = require("./controllers/errorController");
-// const usersController = require("./controllers/usersController");
-// const coursesController = require("./controllers/coursesController");
-// mongoose.connect("mongodb://localhost:27017/ai_academy");
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB :"));
-// db.once("open", () => {
-//   console.log("Connexion réussie à MongoDB avec Mongoose !");
-// });
-
-// const app = express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.use(session({
-//   secret: "ai-academy-secret-key",
-//   resave: false,
-//   saveUninitialized: true
-// }));
-
-// app.use((req, res, next) => {
-//   res.locals.notification = req.session.notification;
-//   delete req.session.notification;
-//   next();
-// });
-
-// app.use(expressLayouts);
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-
-// // Pages publiques
-// app.get("/", homeController.index);
-// app.get("/about", homeController.about);
-// app.get("/courses", homeController.courses);
-// app.get("/faq", homeController.faq);
-// app.get("/contact", homeController.contact);
-// app.post("/contact", homeController.processContact);
-
-
-
-// // Ajouter le middleware method-override
-// const methodOverride = require("method-override");
-// app.use(methodOverride("_method", {
-// methods: ["POST", "GET"]
-// }));
-// // Formulaire d’abonnement
-// app.get("/subscribe", subscribersController.new);
-// app.post("/subscribers", subscribersController.create);
-
-// // Page de remerciement
-// app.get("/subscribers/thanks", (req, res) => {
-//   res.render("subscribers/thanks", { pageTitle: "Merci pour votre inscription" });
-// });
-
-// // Liste et gestion des abonnés
-// app.get("/subscribers/new", subscribersController.new); // IMPORTANT : AVANT `:id`
-// app.get("/subscribers/search", subscribersController.search);
-// app.get("/subscribers", subscribersController.index);
-// app.post("/subscribers/:id/delete", subscribersController.delete);
-// app.get("/subscribers/:id/edit", subscribersController.edit);
-// // Soumet la modification
-// app.post("/subscribers/:id/edit", subscribersController.update);
-// // app.post("/subscribers/:id/update", subscribersController.update);
-// app.get("/subscribers/:id", subscribersController.show); // DOIT ÊTRE EN DERNIER !
-// app.use((req, res, next) => {
-//   res.locals.pageTitle = "AI Academy";
-//   next();
-// });
-
-// // Routes pour les utilisateurs
-// app.get("/users", usersController.index, usersController.indexView);
-// app.get("/users/new", usersController.new);
-// app.post("/users/create", usersController.create, usersController.redirectView);
-// app.get("/users/:id", usersController.show, usersController.showView);
-// app.get("/users/:id/edit", usersController.edit);
-// app.put("/users/:id/update", usersController.update, usersController.redirectView);
-// app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-// // Routes pour les cours
-// app.get("/courses", coursesController.index, coursesController.indexView);
-// app.get("/courses/new", coursesController.new);
-// app.post("/courses/create", coursesController.create, coursesController.redirectView);
-// app.get("/courses/:id", coursesController.show, coursesController.showView);
-// app.get("/courses/:id/edit", coursesController.edit);
-// app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-// app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-// app.use(errorController.error404);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Serveur démarré sur http://localhost:${PORT}`);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const methodOverride = require("method-override");
-
 const app = express();
-
 // Connexion MongoDB
 mongoose.connect("mongodb://localhost:27017/ai_academy");
 const db = mongoose.connection;
@@ -134,7 +12,6 @@ db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB :"));
 db.once("open", () => {
   console.log("Connexion réussie à MongoDB avec Mongoose !");
 });
-
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -152,18 +29,10 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // const methodOverride = require("method-override");
 app.use(methodOverride("_method", {
   methods: ["POST", "GET"]
 }));
-
-
-// Method override pour les formulaires PUT / DELETE
-// app.use(methodOverride("_method", {
-//   methods: ["POST", "GET"]
-// }));
-
 // Configuration de EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
@@ -214,8 +83,7 @@ app.post("/users/create", usersController.create, usersController.redirectView);
 app.get("/users/:id", usersController.show, usersController.showView);
 app.get("/users/:id/edit", usersController.edit);
 app.put("/users/:id/update", usersController.update, usersController.redirectView);
-app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-
+app.delete("/users/:id", usersController.delete);
 
 
 
