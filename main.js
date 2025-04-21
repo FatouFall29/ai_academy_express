8f154090f13e4bf608291e60fac66b691ca878f7
FatouFall29 <fallf5784@gmail.com>
2025-04-21 12:04:52 +0100

    modification du fichier

--- main.js
+++ main.js
@@ -144,53 +144,78 @@ res.locals.loggedIn = req.isAuthenticated();
 res.locals.currentUser = req.user;
 next();
 });
+// Routes d'authentification
+app.get("/login", authController.login);
+app.post("/login", authController.authenticate);
+app.get("/logout", authController.logout, usersController.redirectView);
+app.get("/signup", authController.signup);
+app.post("/signup", authController.register, usersController.redirectView);
+// Routes protégées - accessibles uniquement aux utilisateurs connectés
+app.use("/users", authController.ensureLoggedIn);
+app.use("/courses/new", authController.ensureLoggedIn);
+app.use("/courses/:id/edit", authController.ensureLoggedIn);
+
+
+
+
+
+
+
+
 
 
 
 // 🔹 Routes publiques
-app.get("/", homeController.index);
-app.get("/about", homeController.about);
-app.get("/faq", homeController.faq);
-app.get("/contact", homeController.contact);
-app.post("/contact", homeController.processContact);
+// app.get("/", homeController.index);
+// app.get("/about", homeController.about);
+// app.get("/faq", homeController.faq);
+// app.get("/contact", homeController.contact);
+// app.post("/contact", homeController.processContact);
 
 
 // 🔹 Routes Abonnés (Subscribers)
-app.get("/subscribe", subscribersController.new);
-app.post("/subscribers", subscribersController.create);
-app.get("/subscribers/thanks", (req, res) => {
-  res.render("subscribers/thanks", { pageTitle: "Merci pour votre inscription" });
-});
+// app.get("/subscribe", subscribersController.new);
+// app.post("/subscribers", subscribersController.create);
+// app.get("/subscribers/thanks", (req, res) => {
+//   res.render("subscribers/thanks", { pageTitle: "Merci pour votre inscription" });
+// });
 
-app.get("/subscribers/new", subscribersController.new);
-app.get("/subscribers/search", subscribersController.search);
-app.get("/subscribers", subscribersController.index);
-app.post("/subscribers/:id/delete", subscribersController.delete);
-app.get("/subscribers/:id/edit", subscribersController.edit);
-app.post("/subscribers/:id/edit", subscribersController.update);
-app.get("/subscribers/:id", subscribersController.show);
+
+
+
+// app.get("/subscribers/new", subscribersController.new);
+// app.get("/subscribers/search", subscribersController.search);
+// app.get("/subscribers", subscribersController.index);
+// app.post("/subscribers/:id/delete", subscribersController.delete);
+// app.get("/subscribers/:id/edit", subscribersController.edit);
+// app.post("/subscribers/:id/edit", subscribersController.update);
+// app.get("/subscribers/:id", subscribersController.show);
+
+
+
 
 
 // 🔹 Routes Utilisateurs (Users)
-app.get("/users", usersController.index, usersController.indexView);
-app.get("/users/new", usersController.new);
-app.post("/users/create", usersController.create, usersController.redirectView);
-app.get("/users/:id", usersController.show, usersController.showView);
-app.get("/users/:id/edit", usersController.edit);
-app.put("/users/:id/update", usersController.update, usersController.redirectView);
-app.delete("/users/:id", usersController.delete);
+// app.get("/users", usersController.index, usersController.indexView);
+// app.get("/users/new", usersController.new);
+// app.post("/users/create", usersController.create, usersController.redirectView);
+// app.get("/users/:id", usersController.show, usersController.showView);
+// app.get("/users/:id/edit", usersController.edit);
+// app.put("/users/:id/update", usersController.update, usersController.redirectView);
+// app.delete("/users/:id", usersController.delete);
+
+
 
 
 
 // 🔹 Routes Cours (Courses)
-app.get("/courses", coursesController.index, coursesController.indexView);
-app.get("/courses/new", coursesController.new);
-app.post("/courses/create", coursesController.create, coursesController.redirectView);
-app.get("/courses/:id", coursesController.show, coursesController.showView);
-app.get("/courses/:id/edit", coursesController.edit);
-app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-// app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-app.delete("/courses/:id", coursesController.delete, coursesController.redirectView);
+// app.get("/courses", coursesController.index, coursesController.indexView);
+// app.get("/courses/new", coursesController.new);
+// app.post("/courses/create", coursesController.create, coursesController.redirectView);
+// app.get("/courses/:id", coursesController.show, coursesController.showView);
+// app.get("/courses/:id/edit", coursesController.edit);
+// app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+// app.delete("/courses/:id", coursesController.delete, coursesController.redirectView);
 
 
 // Gestion des erreurs
