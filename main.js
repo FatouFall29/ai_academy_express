96ad542ed3bf3e398f515d08e556a39975c5d3cd
FatouFall29 <fallf5784@gmail.com>
2025-04-21 11:45:16 +0100

    modification du fichier

--- main.js
+++ main.js
@@ -1,55 +1,150 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const expressLayouts = require("express-ejs-layouts");
-const session = require("express-session");
-const methodOverride = require("method-override");
-const app = express();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const expressLayouts = require("express-ejs-layouts");
+// const session = require("express-session");
+// const methodOverride = require("method-override");
+// const app = express();
+
+
+
+
 // Connexion MongoDB
-mongoose.connect("mongodb://localhost:27017/ai_academy");
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB :"));
-db.once("open", () => {
-  console.log("Connexion réussie à MongoDB avec Mongoose !");
-});
+// mongoose.connect("mongodb://localhost:27017/ai_academy");
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB :"));
+// db.once("open", () => {
+//   console.log("Connexion réussie à MongoDB avec Mongoose !");
+// });
+
+
+
+
 // Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  secret: "ai-academy-secret-key",
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: "ai-academy-secret-key",
+//   resave: false,
+//   saveUninitialized: true
+// }));
 
-app.use((req, res, next) => {
-  res.locals.notification = req.session.notification;
-  delete req.session.notification;
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.notification = req.session.notification;
+//   delete req.session.notification;
+//   next();
+// });
+
+
+
+
+
 
 // const methodOverride = require("method-override");
-app.use(methodOverride("_method", {
-  methods: ["POST", "GET"]
-}));
+// app.use(methodOverride("_method", {
+//   methods: ["POST", "GET"]
+// }));
+
+
+
+
 // Configuration de EJS
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// app.use(expressLayouts);
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+
+
+
 
 // Définir le titre global
-app.use((req, res, next) => {
-  res.locals.pageTitle = "AI Academy";
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.pageTitle = "AI Academy";
+//   next();
+// });
+
+
+
 
 // Importation des contrôleurs
-const subscribersController = require("./controllers/subscribersController");
+// const subscribersController = require("./controllers/subscribersController");
+// const homeController = require("./controllers/homeController");
+// const errorController = require("./controllers/errorController");
+// const usersController = require("./controllers/usersController");
+// const coursesController = require("./controllers/coursesController");
+
+
+
+
+
+
+
+
+
+const express = require("express");
+const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
+const subscribersController = require("./controllers/subscribersController");
 const usersController = require("./controllers/usersController");
 const coursesController = require("./controllers/coursesController");
+// Configuration de la connexion à MongoDB
+mongoose.connect(
+"mongodb://localhost:27017/ai_academy",
+{ useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
+});
+const app = express();
+// Configuration de l'application
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(layouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method", {
+methods: ["POST", "GET"]
+}));
+// Configuration des cookies et des sessions
+app.use(cookieParser("secret_passcode"));
+app.use(session({
+secret: "secret_passcode",
+cookie: {
+maxAge: 4000000
+},
+resave: false,
+saveUninitialized: false
+}));
+// Configuration de flash messages
+app.use(flash());
+// Configuration de Passport
+app.use(passport.initialize());
+app.use(passport.session());
+// Configuration du User model pour Passport
+const User = require("./models/user");
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// Middleware pour rendre les variables locales disponibles dans toutes les vues
+app.use((req, res, next) => {
+res.locals.flashMessages = req.flash();
+res.locals.loggedIn = req.isAuthenticated();
+res.locals.currentUser = req.user;
+next();
+});
+
 
 
 // 🔹 Routes publiques
@@ -98,8 +193,11 @@ app.put("/courses/:id/update", coursesController.update, coursesController.redir
 app.delete("/courses/:id", coursesController.delete, coursesController.redirectView);
 
 
-// 🔹 Gestion des erreurs
+// Gestion des erreurs
 app.use(errorController.error404);
+
+
+
 
 // Lancement du serveur
 const PORT = process.env.PORT || 3000;
